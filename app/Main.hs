b4a64bed9cf03ee97f565a5c156d124d61caa100
Mario Rogic <hello@mario.net.au>
2017-07-17 23:01:33 +0100

    Extend sample app and add run instructions

--- app/Main.hs
+++ app/Main.hs
@@ -1,23 +1,57 @@
 module Main where
 
+import           TextShow
 import           Data.Monoid        ((<>))
 import qualified Hilt
-import qualified Hilt.Channel as Channel
-import qualified Hilt.Logger  as Logger
+import qualified Hilt.Server
+import qualified Hilt.Channel      as Channel
+import qualified Hilt.Logger       as Logger
+import qualified Hilt.SocketServer as Websocket
 
+{-
+To run this sample locally:
+
+```
+git clone git@github.com:supermario/hilt.git
+cd hilt && stack build && stack exec hilt-example
+
+# In another terminal window
+curl -i -N -H "Connection: Upgrade" -H "Upgrade: websocket" -H "Sec-WebSocket-Key: SGVsbG8sIHdvcmxkIQ==" localhost:8081
+```
+-}
 main :: IO ()
 main = Hilt.manage $ do
 
   logger <- Logger.load
   chan   <- Channel.load
 
+  let
+    onJoined :: Websocket.OnJoined
+    onJoined clientId clientCount = do
+      Logger.debug logger $ showt clientId <> " joined, " <> showt clientCount <> " connected."
+      return $ Just "Hello client!"
+
+    onReceive :: Websocket.OnReceive
+    onReceive clientId text = do
+      Logger.debug logger $ showt clientId <> " said " <> showt text
+      Channel.write chan text
+
+  websocket <- Websocket.load onJoined onReceive
+
+
   -- Now we can write our business logic using our services
   Hilt.program $ do
     Logger.debug logger "Starting up!"
 
-    Channel.worker chan (\text -> Logger.debug logger ("Received message: " <> text))
+    -- Log all messages received, then broadcast back to all clients
+    Channel.worker chan (\text -> do
+        Logger.debug logger ("[worker] got " <> text)
+        Websocket.broadcast websocket text
+      )
 
     Channel.write chan "Hello world!"
 
     -- Or pass services off to some other areas of your app
     -- someMoreLogic logger chan
+
+    Hilt.Server.runWebsocket websocket
